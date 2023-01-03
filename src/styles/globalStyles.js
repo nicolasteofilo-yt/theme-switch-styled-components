5c4a3a868896ab96fc3c7df4b52cb296d0541bf8
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2023-01-03 14:39:43 -0300

    chore: add styled-components and create global styles

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  html {
+    scroll-behavior: smooth;
+    box-sizing: inherit;
+    width: 100%;
+  }
+
+  body {
+    margin: 0 auto;
+    background-color: #0D1117;
+    color: #E1E1E1;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+
+  ul, li, ol {
+    list-style: none;
+  }
+`;
